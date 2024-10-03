a9511bcb15fa45a0a483d177a0d94a2266da2570
Alex <alexvnep@gmail.com>
2024-10-03 11:28:48 +0200

    Make event handler query the location and date1 and pass those values to data and call populateLocale

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import { populateLocale } from "./populateData";
 
 const fetchBtn = document.querySelector("#fetchBtn");
 
-fetchBtn.addEventListener("click", (event) => {
-  getWeatherData();
-  populateLocale();
+fetchBtn.addEventListener("click", () => {
+  const location = document.querySelector("#location");
+  const date1 = document.querySelector("#date1");
+  const data = getWeatherData(location.value, date1.value);
+  populateLocale(data);
 });
