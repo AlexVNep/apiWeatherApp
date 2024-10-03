f4d9ba25e7c7796a0c067fd220cf784c551178d2
Alex <alexvnep@gmail.com>
2024-10-03 11:30:49 +0200

    Add date  holder query and set locale and date textContent by passing getWeatherData as a param (data)

--- src/populateData.js
+++ src/populateData.js
@@ -1,16 +1,12 @@
-import getWeatherData from "./weatherData";
-
-const data = await getWeatherData(location.value);
-
-function populateLocale() {
+async function populateLocale(data) {
   if (data) {
-    const location = document.querySelector("#location");
     const locale = document.querySelector("#locale");
-    locale.textContent = data[location];
+    locale.textContent = data["location"];
+    const date = document.querySelector("#date");
+    date.textContent = data["date1"];
   }
 }
 
-// const date1 = document.querySelector("#date1");
 // const tempmax = document.querySelector("#temp");
 
 export { populateLocale };
