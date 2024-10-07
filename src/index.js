e319f64e19ad633bf430b83675abaa8e397c1247
Alex <alexvnep@gmail.com>
2024-10-07 10:06:21 +0200

    Add event handler for unit change and add value to getWeatherData

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import "./styles.css";
 import getWeatherData from "./weatherData";
 import { populateLocale } from "./populateData";
+import unitSelection from "./unitSelection";
 
 const fetchBtn = document.querySelector("#fetchBtn");
+const unit = document.querySelector("#unit");
 
 fetchBtn.addEventListener("click", async () => {
   const location = document.querySelector("#location");
   const date1 = document.querySelector("#date1");
-  const data = await getWeatherData(location.value, date1.value);
+
+  const data = await getWeatherData(location.value, date1.value, unit.value);
+
   populateLocale(data);
 });
+
+unit.addEventListener("change", unitSelection);
