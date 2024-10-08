6874a199b7b9a74891dcb7e79da4213cf4c86a9d
Alex <alexvnep@gmail.com>
2024-10-08 10:14:57 +0200

    Add func to load weather data for a default city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./styles.css";
-import getWeatherData from "./weatherData";
+import getWeatherData, { getWeatherGif } from "./weatherData";
 import { populateData, populateGif } from "./populateData";
 import unitSelection from "./unitSelection";
-import { getWeatherGif } from "./weatherData";
+
+async function onLoad() {
+  const data = await getWeatherData("benoni", "2024-10-08", "metric");
+  const gifData = await getWeatherGif(data.condition);
+  populateData(data);
+  populateGif(gifData);
+}
+
+onLoad();
 
 const fetchBtn = document.querySelector("#fetchBtn");
 const unit = document.querySelector("#unit");
@@ -10,7 +18,6 @@ const unit = document.querySelector("#unit");
 fetchBtn.addEventListener("click", async () => {
   const location = document.querySelector("#location");
   const date1 = document.querySelector("#date1");
-
   const data = await getWeatherData(location.value, date1.value, unit.value);
   const gifData = await getWeatherGif(data.condition);
 
